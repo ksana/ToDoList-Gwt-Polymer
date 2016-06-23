3e65c8192408c018fe8ec4a1c0c2664724f46134
ksana <oxana.ou@gmail.com>
2016-06-23 16:49:20 +0200

    added Item file

--- src/main/java/org/gwtproject/tutorial/client/Main.java
+++ src/main/java/org/gwtproject/tutorial/client/Main.java
@@ -1,9 +1,16 @@
 package org.gwtproject.tutorial.client;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.vaadin.polymer.paper.widget.PaperDialog;
+import com.vaadin.polymer.paper.widget.PaperDrawerPanel;
+import com.vaadin.polymer.paper.widget.PaperInput;
+import com.vaadin.polymer.paper.widget.PaperTextarea;
 
 public class Main extends Composite {
     interface MainUiBinder extends UiBinder<HTMLPanel, Main> {
@@ -11,7 +18,42 @@ public class Main extends Composite {
 
     private static MainUiBinder ourUiBinder = GWT.create(MainUiBinder.class);
 
+    @UiField
+    PaperDrawerPanel drawerPanel;
+    @UiField
+    HTMLPanel content;
+
+    @UiField
+    PaperDialog addItemDialog;
+    @UiField
+    PaperInput titleInput;
+    @UiField
+    PaperTextarea descriptionInput;
+
     public Main() {
         initWidget(ourUiBinder.createAndBindUi(this));
     }
+
+    @UiHandler("addButton")
+    protected void onAddButtonClick(ClickEvent e) {
+        addItemDialog.open();
+    }
+
+    @UiHandler("confirmAddButton")
+    protected void onConfirmAddButtonClick(ClickEvent e) {
+        if (!titleInput.getValue().isEmpty()) {
+            addItem(titleInput.getValue(), descriptionInput.getValue());
+            // clear text fields
+            titleInput.setValue("");
+            descriptionInput.setValue("");
+        }
+    }
+
+    private void addItem(String title, String description) {
+        Item item = new Item();
+        item.setTitle(title);
+        item.setDescription(description);
+        content.add(item);
+    }
+
 }
